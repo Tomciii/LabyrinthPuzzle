a648fc11428651b5c2365a3e730c8a1cca89d103
Tomciii <69507140+Tomciii@users.noreply.github.com>
2023-04-08 13:16:27 +0200

    #2 - Update EightTilePuzzleScreen
    #2 - Update EightTilePuzzleScreen

--- app/src/main/java/com/example/labyrinthpuzzle/screens/EightTilePuzzleScreen.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/screens/EightTilePuzzleScreen.kt
@@ -2,13 +2,16 @@ package com.example.labyrinthpuzzle.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -16,6 +19,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.labyrinthpuzzle.models.getEightTiles
 import com.example.labyrinthpuzzle.widgets.HomeTopAppBar
+import java.lang.Math.abs
 
 @Composable
 fun EightTilePuzzleScreen(navController: NavController = rememberNavController()){
@@ -29,6 +33,10 @@ fun EightTilePuzzleScreen(navController: NavController = rememberNavController()
 
 @Composable
 fun EightTileScreen(eightTile: Array<Array<Int>>) {
+    var emptyTilePosition by remember { mutableStateOf(0 to 0) }
+    var tiles by remember { mutableStateOf(eightTile) }
+    var selectedTile by remember { mutableStateOf(-1 to -1) }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -37,7 +45,6 @@ fun EightTileScreen(eightTile: Array<Array<Int>>) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Eight Tile Screen")
         Spacer(modifier = Modifier.height(16.dp))
         Box(
             modifier = Modifier
@@ -48,35 +55,68 @@ fun EightTileScreen(eightTile: Array<Array<Int>>) {
             var num = 365 / 10
             val boxSize = with(LocalDensity.current) { (num.dp).toPx() }
             val textSize = with(LocalDensity.current) { (boxSize / 1).toSp() }
-           Column(
-               modifier = Modifier.fillMaxSize()
-           ){
-            for (i in eightTile.indices) {
-                Row(
-                    modifier = Modifier
-                        .height(boxSize.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    for (j in eightTile[i].indices) {
-                        Box(
-                            modifier = Modifier
-                                .weight(2f)
-                                .aspectRatio(1f)
-                                .border(1.dp, Color.Black),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(
-                                text = eightTile[i][j].toString(),
-                                fontSize = textSize,
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis
-                            )
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ){
+                for (i in tiles.indices) {
+                    Row(
+                        modifier = Modifier
+                            .height(boxSize.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        for (j in tiles[i].indices) {
+                            val position = i to j
+
+                            val emptyTileRow = emptyTilePosition.first
+                            val emptyTileCol = emptyTilePosition.second
+                            val isSelectedTile = selectedTile == position
+                            val backgroundColor = if (tiles[i][j] == 0) Color.White else if (isSelectedTile) Color.White else Color.LightGray
+                            val isAdjacentToEmptyTile = (abs(i - emptyTileRow) == 1 && j == emptyTileCol) ||
+                                    (abs(j - emptyTileCol) == 1 && i == emptyTileRow)
+                            Box(
+                                modifier = Modifier
+                                    .weight(2f)
+                                    .aspectRatio(1f)
+                                    .background(backgroundColor)
+                                    .border(1.dp, Color.Black)
+                                    .pointerInput(Unit) {
+                                        detectTapGestures(
+                                            onPress = {
+                                                selectedTile = position
+                                            },
+                                            onTap = {
+                                                if (isAdjacentToEmptyTile) {
+                                                    swapTiles(position, emptyTilePosition, tiles)
+                                                    emptyTilePosition = position
+                                                }
+                                                selectedTile = -1 to -1
+                                            }
+                                        )
+                                    },
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = tiles[i][j].toString(),
+                                    fontSize = textSize,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                    color = Color.Black
+                                )
+                            }
+                            if (tiles[i][j] == 0) {
+                                emptyTilePosition = i to j
+                            }
                         }
                     }
                 }
             }
-           }
         }
     }
+}
+
+private fun swapTiles(position1: Pair<Int, Int>, position2: Pair<Int, Int>, tiles: Array<Array<Int>>) {
+    val temp = tiles[position1.first][position1.second]
+    tiles[position1.first][position1.second] = tiles[position2.first][position2.second]
+    tiles[position2.first][position2.second] = temp
 }
