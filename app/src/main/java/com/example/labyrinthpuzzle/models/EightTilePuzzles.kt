6253a5138d1397661215b64bfee4e97e0b63a449
tomcii <116713@fhwn.ac.at>
2023-04-18 23:43:22 +0200

    Update EightTilePuzzles.kt
    Update

--- app/src/main/java/com/example/labyrinthpuzzle/models/EightTilePuzzles.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/models/EightTilePuzzles.kt
@@ -5,7 +5,8 @@ import androidx.room.Entity
 // @Entity
 data class EightTile (
     var id: Int,
-    var grid: Array<Array<Int>>
+    var grid: Array<Array<Int>>,
+    var isSolved: Boolean
 )
 
 var eightTiles = listOf(
@@ -14,7 +15,8 @@ var eightTiles = listOf(
             arrayOf(6, 0, 8),
             arrayOf(3, 7, 2),
             arrayOf(5, 1, 4))
-))
+, isSolved = false
+    ))
 
 fun getEightTiles(id:Int): Array<Array<Int>>{
     return eightTiles.get(id).grid
