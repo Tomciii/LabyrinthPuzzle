6d463fe7e21517bad23e6ca167360f82c88bb6b8
tomcii <116713@fhwn.ac.at>
2023-04-24 18:04:57 +0200

    Add EightTiles
    Add EightTiles

--- app/src/main/java/com/example/labyrinthpuzzle/models/EightTilePuzzle.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/models/EightTilePuzzle.kt
@@ -14,9 +14,20 @@ data class EightTile (
 var eightTiles = listOf(
     EightTile(
         id = 1,
-        grid = listOf("0","1","2","3","4","5","6","7","8"),
+        grid = listOf("1","0","2","3","4","5","6","7","8"),
         isSolved = false
-    ))
+    ),
+    EightTile(
+        id = 2,
+        grid = listOf("3","1","2","4","0","5","6","7","8"),
+        isSolved = false
+    ),
+    EightTile(
+        id = 3,
+        grid = listOf("1","2","3","0","4","5","6","7","8"),
+        isSolved = false
+    )
+)
 
 fun getEightTilesPuzzle(id:Int): List<String>{
     return eightTiles.get(id).grid
