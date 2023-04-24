057131e9c7b863938dd782c0aa7685cb02dfb4ed
tomcii <116713@fhwn.ac.at>
2023-04-24 18:19:08 +0200

    Injectorutils update
    Injectorutils update

--- app/src/main/java/com/example/labyrinthpuzzle/utils/InjectorUtils.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/utils/InjectorUtils.kt
@@ -2,8 +2,11 @@ package com.example.labyrinthpuzzle.utils
 
 import android.content.Context
 import com.example.labyrinthpuzzle.data.EightTilePuzzleDatabase
+import com.example.labyrinthpuzzle.data.MemoryPuzzleDatabase
 import com.example.labyrinthpuzzle.repository.EightTilePuzzleRepository
+import com.example.labyrinthpuzzle.repository.MemoryPuzzleRepository
 import com.example.labyrinthpuzzle.viewModels.factories.EightTilesPuzzleViewModelFactory
+import com.example.labyrinthpuzzle.viewModels.factories.MemoryPuzzleViewModelFactory
 
 object InjectorUtils {
 
@@ -14,5 +17,14 @@ object InjectorUtils {
     fun provideEightTilePuzzleViewModel(context: Context) : EightTilesPuzzleViewModelFactory {
         val repository = getEightPuzzleTileRepository(context)
         return EightTilesPuzzleViewModelFactory(repository)
+    }
+
+    private fun getMemoryRepository(context: Context) : MemoryPuzzleRepository {
+        return MemoryPuzzleRepository(MemoryPuzzleDatabase.getDatabase(context).memoryPuzzleDao())
+    }
+
+    fun provideMemoryPuzzleViewModel(context: Context) : MemoryPuzzleViewModelFactory {
+        val repository = getMemoryRepository(context)
+        return MemoryPuzzleViewModelFactory(repository)
     }
 }
