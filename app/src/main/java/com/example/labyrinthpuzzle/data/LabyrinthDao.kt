88cb01c91bba2cc07e0d94b58305cb111ffbd26d
Tomciii <69507140+Tomciii@users.noreply.github.com>
2023-05-14 14:57:23 +0200

    Update LabyrinthDao.kt

--- app/src/main/java/com/example/labyrinthpuzzle/data/LabyrinthDao.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/data/LabyrinthDao.kt
@@ -1,14 +1,16 @@
 package com.example.labyrinthpuzzle.data
 
+import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Update
 import com.example.labyrinthpuzzle.models.Labyrinth
 
+@Dao
 interface LabyrinthDao {
 
     @Update
     suspend fun update(labyrinth: Labyrinth)
 
     @Query("SELECT * FROM labyrinth where id=:id")
-    fun getLabyrinthTileById(id: Int): Labyrinth            //should it get a tile?
+    fun getLabyrinthById(id: Int): Labyrinth            //should it get a tile?
 }
