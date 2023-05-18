f7b771f0280f583e46bd8bd169258a0015f3947d
Mariam Khreis <mariam-khreis@hotmail.com>
2023-05-18 23:52:12 +0200

    Labyrinth Tile Repository

--- app/src/main/java/com/example/labyrinthpuzzle/model/repository/LabyrinthRepository.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/model/repository/LabyrinthRepository.kt
@@ -1,6 +1,14 @@
 package com.example.labyrinthpuzzle.model.repository
 
 import com.example.labyrinthpuzzle.model.dao.LabyrinthDao
+import com.example.labyrinthpuzzle.model.models.LabyrinthTile
 
 class LabyrinthRepository(private val labyrinthDao: LabyrinthDao) {
+
+    // fun getLabyrinthTileById(id: Int) = labyrinthDao.getLabyrinthById(id)
+
+    fun getLabyrinthTileById(id: Int): LabyrinthTile {
+        return labyrinthDao.getLabyrinthById(id)
+    }
+
 }
