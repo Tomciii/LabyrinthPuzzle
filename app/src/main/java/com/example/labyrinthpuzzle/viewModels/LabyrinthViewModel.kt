7d9da30e6977b255636c9c89c8b6f8433d8b0a49
Mariam Khreis <mariam-khreis@hotmail.com>
2023-05-19 15:25:38 +0200

    Labyrinth ViewModel get labyrinth tile function

--- app/src/main/java/com/example/labyrinthpuzzle/viewModels/LabyrinthViewModel.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/viewModels/LabyrinthViewModel.kt
@@ -1,8 +1,20 @@
 package com.example.labyrinthpuzzle.viewModels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.labyrinthpuzzle.model.models.LabyrinthTile
 import com.example.labyrinthpuzzle.model.repository.LabyrinthRepository
 
 class LabyrinthViewModel(private val repository: LabyrinthRepository): ViewModel() {
 
+    private var labyrinthTile = MutableLiveData<LabyrinthTile>()                                   //to observe changes to the labyrinth tile by observing the labyrinthTile live data
+
+    fun getLabyrinthTileById(id: Int): LabyrinthTile? {
+        return repository.getLabyrinthTileById(id)
+    }
+
+    /* fun loadLabyrinthTileById(id: Int) {
+        val tile = repository.getLabyrinthTileById(id)
+        labyrinthTile.value = tile
+    } */
 }
