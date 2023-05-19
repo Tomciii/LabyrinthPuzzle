3538c12d4ca5450696002566a09d4a38998238e8
tomcii <116713@fhwn.ac.at>
2023-05-19 20:42:53 +0200

    Add TopAppBar

--- app/src/main/java/com/example/labyrinthpuzzle/view/screen/SettingsScreen.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/view/screen/SettingsScreen.kt
@@ -1,10 +1,20 @@
 package com.example.labyrinthpuzzle.screen
 
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.labyrinthpuzzle.view.screen.EightTileScreen
+import com.example.labyrinthpuzzle.view.widgets.SimpleTopAppBar
 
 @Composable
 fun SettingsScreen(navController: NavController = rememberNavController()){
+    Surface {
+        SimpleTopAppBar(arrowBackClicked = { navController.popBackStack() }) {
+            Text(text = "Settings")
+        }
 
+        // Settings kek
+    }
 }
