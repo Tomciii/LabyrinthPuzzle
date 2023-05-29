526f1b68656bd651368354b9c7dc25ce0a750446
tomcii <116713@fhwn.ac.at>
2023-05-29 21:08:40 +0200

    Memory Change

--- app/src/main/java/com/example/labyrinthpuzzle/model/models/MemoryPuzzle.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/model/models/MemoryPuzzle.kt
@@ -8,6 +8,5 @@ data class Memory (
     @PrimaryKey(autoGenerate = true)
     var id: Int,
     var grid: List<String>, // [1,1,2,2,3,3] pairs of 2, 6 puzzle pieces in total
-    var uncoveredState: List<String>, // [0,0,0,0,0,0] 0 = hidden, 1 = turned around
     var isSolved: Boolean
 )
