3d61253dae65d3ab398fdcc20446d6a2c1a88bc5
tomcii <116713@fhwn.ac.at>
2023-06-03 23:04:19 +0200

    Add MemoryPuzzleScreen Stuff

--- app/src/main/java/com/example/labyrinthpuzzle/view/screen/MemoryPuzzleScreen.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/view/screen/MemoryPuzzleScreen.kt
@@ -1,9 +1,139 @@
 package com.example.labyrinthpuzzle.view.screen
 
-import androidx.compose.runtime.Composable
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.labyrinthpuzzle.model.models.Eight
+import com.example.labyrinthpuzzle.model.models.Memory
+import com.example.labyrinthpuzzle.model.utils.InjectorUtils
+import com.example.labyrinthpuzzle.view.widgets.SimpleTopAppBar
+import com.example.labyrinthpuzzle.viewModels.MemoryPuzzleViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun MemoryPuzzleScreen (navController: NavController, memoryPuzzleId: String?){
+    val viewModel: MemoryPuzzleViewModel =
+        viewModel(factory = InjectorUtils.provideMemoryPuzzleViewModel(LocalContext.current))
+
+    val updatedMemoryPuzzleID = rememberUpdatedState(memoryPuzzleId)
+
+    var memoryPuzzle by remember { mutableStateOf<Memory?>(null) }
+
+    LaunchedEffect(updatedMemoryPuzzleID.value) {
+        withContext(Dispatchers.IO) {
+            memoryPuzzle = viewModel.getMemoryPuzzleById(updatedMemoryPuzzleID.value.toString())
+        }
+    }
+
+    Surface {
+        SimpleTopAppBar(arrowBackClicked = { navController.popBackStack() }) {
+            Text(text = "Memory Puzzle")
+        }
+        memoryPuzzle?.let {MemoryPuzzle(memoryPuzzle, navController, viewModel) }
+    }
 
 }
+
+@SuppressLint("CoroutineCreationDuringComposition", "UnrememberedMutableState",
+    "StateFlowValueCalledInComposition"
+)
+@Composable
+fun MemoryPuzzle(
+    memoryPuzzle: Memory?,
+    navController: NavController,
+    viewModel: MemoryPuzzleViewModel
+){
+    var memoryPuzzleInstance by remember {
+        mutableStateOf(memoryPuzzle)
+    }
+
+    val solvedArrayState = MutableStateFlow(mutableStateListOf<Int?>(0, 0, 0, 0, 0, 0).toTypedArray())
+    var solvedArray = solvedArrayState.value
+
+
+    val coroutineScope = rememberCoroutineScope()
+
+    val rows = 3
+    val columns = 2
+
+    val grid = Array(rows) { IntArray(columns) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .height(450.dp)
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.align(Alignment.Center)
+            ){
+            for (i in 0 until rows) {
+                Row(
+                    modifier = Modifier.padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    for (j in 0 until columns) {
+                            Box(
+                                modifier = Modifier
+                                    .size(130.dp)
+                                    .padding(8.dp)
+                                    .background(Color.Gray, RectangleShape)
+                                    .clickable {
+
+                                        grid[i][j] = i * columns + j
+                                    }
+                            ) {
+                                Text(
+                                    text = "Box ($i, $j)",
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                    }
+                }
+            }
+            }
+        }
+
+        if (viewModel.isPuzzleInCorrectOrder(solvedArray)) {
+            Button(onClick = { navController.popBackStack() }, enabled = true) {
+                Text("Puzzle Solved!")
+            }
+
+            memoryPuzzleInstance!!.isSolved = true
+            var solvedPuzzle = Memory(memoryPuzzleInstance!!.id, memoryPuzzle!!.grid, true)
+
+            coroutineScope.launch {
+          //      viewModel.update(solvedPuzzle)
+            }
+
+        } else {
+            Button(onClick = { navController.popBackStack() }, enabled = false) {
+                Text("Puzzle not solved")
+            }
+        }
+}}
--- app/src/main/java/com/example/labyrinthpuzzle/viewModels/MemoryPuzzleViewModel.kt
+++ app/src/main/java/com/example/labyrinthpuzzle/viewModels/MemoryPuzzleViewModel.kt
@@ -15,4 +15,12 @@ class MemoryPuzzleViewModel(private val repository: MemoryPuzzleRepository) : Vi
     fun isPuzzleSolved(id: Int): Boolean {
         return repository.isSolved(id)
     }
+
+    fun isPuzzleInCorrectOrder(arr: Array<Int?>): Boolean {
+       return arr[0] == 1 && arr[1] == 1 && arr[2] == 1 && arr[3] == 1 && arr[4] == 1 && arr[5] == 1
+    }
+
+    suspend fun update(memory: Memory){
+        repository.update(memory)
+    }
 }
